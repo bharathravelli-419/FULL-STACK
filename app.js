786d21645e610ef628886c38c985f04676287d3d
Bharath Ravelli <bharathravelli419@gmail.com>
2021-10-30 22:41:23 +0530

    var and null etc.

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-//Strings
-console.log(typeof "hello world")
-console.log(typeof 'd')
-
-
-//numbers
-console.log(typeof 1.2)
-console.log(typeof -1.55556)
-
-//boolean
-console.log(typeof true)
-
-console.log(typeof null)
+var age = 'bharath ravelli'
+console.log(typeof age)
