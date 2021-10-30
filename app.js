3096467af3ee1c6c794cdf1b6c1e4f384164f791
Bharath Ravelli <bharathravelli419@gmail.com>
2021-10-30 20:15:18 +0530

    primitive data types JS

---
+++ app.js
@@ -0,0 +1,13 @@
+//Strings
+console.log(typeof "hello world")
+console.log(typeof 'd')
+
+
+//numbers
+console.log(typeof 1.2)
+console.log(typeof -1.55556)
+
+//boolean
+console.log(typeof true)
+
+console.log(typeof null)
