f21df7a88f3708d67a70f0765afe30d20ca47c84
Bharath Ravelli <bharathravelli419@gmail.com>
2021-10-30 22:52:39 +0530

    basic stuff

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var age = 'bharath ravelli'
-console.log(typeof age)
+var age =[2,2,3,5,'bharath ravelli']
+console.log(typeof age[4])
+console.log(age.length)
+console.log(age)
+age.push(3)
+console.groupCollapsed(age)
+console.log(age.length)
+const var1=3;
+var a,b
+a=21
+b=23
+console.log(a,b)
