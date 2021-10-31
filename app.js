fb60c405d88ea33b2ad7fc6a0214a6fcebe132c6
Bharath Ravelli <bharathravelli419@gmail.com>
2021-10-31 16:13:31 +0530

    Dark mode

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+var body = document.querySelector('body')
 
 const callback =(event)=>{
-    const inputValue=input.value
-    const element=document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
-    console.log(event.target)
-    console.log(input.value)
+         body.classList.toggle('dark')   
 }
 
 button.addEventListener('click',callback)
