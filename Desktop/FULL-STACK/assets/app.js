02d4dc6bc4bd1791e3f0730822c3c9daa3b9751a
Bharath Ravelli <bharathravelli419@gmail.com>
2021-12-06 21:56:59 +0530

    functions in java script

--- Desktop/FULL-STACK/assets/app.js
+++ Desktop/FULL-STACK/assets/app.js
@@ -1,26 +1,19 @@
-var head = document.getElementById('heading')
-head.innerHTML="changed"
-var paragraph= document.getElementsByClassName('para')
-console.log(head)
-console.log(paragraph.length)
-console.log(typeof head)
-
-
-var tags  = document.getElementsByTagName('body')
-console.log(tags[0])
-var query = document.querySelector('.para')
-console.log( query)
-head.style="color:red;background-color:yellow;font-size:2rem"
-var query1 = document.querySelector('body')
-query1.classList.add('dark')
-console.log(head.innerHTML)
-
-var list= document.querySelector('ol')
-var arr =['go to gym','cook oatmeal','eat']
-  for(var i =0;i<arr.length;i++)
-{
-    var element = document.createElement('li')
-    var textnode =document.createTextNode(arr[i])
-     element.appendChild(textnode)
-     list.appendChild(element)
+function greet(name,age=19){
+    console.log('hello'+' '+name+'age is: '+age)
 }
+greet('bharath')
+function sum(a,b){
+    return a+b
+}
+var sum= sum(10,20)
+console.log(sum)
+
+var subtract = function (a,b){
+    return a-b
+}
+
+console.log(subtract(10,2))
+var greet1= (a,b)=>{
+    console.log('greetings to ShapeAI')
+}
+greet1()
