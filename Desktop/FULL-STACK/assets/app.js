5a9e5925dc1fc108ad3911e41b4daa1b06a3a160
Bharath Ravelli <bharathravelli419@gmail.com>
2021-12-09 02:10:52 +0530

    setTimeout() function

--- Desktop/FULL-STACK/assets/app.js
+++ Desktop/FULL-STACK/assets/app.js
@@ -1,14 +1,4 @@
-console.log(this)
-let car ={
-      Name:"C class",
-      manufacturer:"mercedes",
-    print1: function (a) {
-        console.log(this)  
-        console.log("car function " +a+ this.Name +" "+car.manufacturer)
-      }
-}
-console.log(typeof car)
-car.print1(' bharath ravelli ')
-console.log(Object.keys(car))
-console.log(Object.values(car))
-console.log(car.hasOwnProperty('Name'))
+setTimeout(()=>{
+    console.log("bhavani")
+},5000)
+console.log('HI')
