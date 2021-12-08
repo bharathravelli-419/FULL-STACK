8080b58d7ac9da786c7b43146fa4e5fff81cad8f
Bharath Ravelli <bharathravelli419@gmail.com>
2021-12-08 23:46:00 +0530

    calculator function

--- Desktop/FULL-STACK/assets/app.js
+++ Desktop/FULL-STACK/assets/app.js
@@ -1,8 +1,29 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const multiply= document.querySelector('#multiply')
+const resultbox = document.querySelector('.result')
+console.log(a,b)
 
-callbackfunc=(event)=>
-{
-    body.classList.toggle('dark')
+/*
+const sum=()=>{
+    const result= parseInt(a.value)+parseInt(b.value)
+    resultbox.innerHTML= result
 }
-button.addEventListener('click',callbackfunc)
+add.addEventListener('click',sum)
+const product=()=>{
+    const result= parseInt(a.value)*parseInt(b.value)
+    resultbox.innerHTML= result
+}
+multiply.addEventListener('click',product)
+
+ */
+const calculate =(event,operator)=>{
+    console.log(event.target + operator)
+}
+add.addEventListener('click',(event)=>{
+    calculate(event,'add')
+})
+multiply.addEventListener('click',(event)=>{
+    calculate(event,'multiply')
+})
