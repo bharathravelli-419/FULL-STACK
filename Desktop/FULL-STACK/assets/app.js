7ed5d366f476c5ff1a3ffbf34b226992e9642f41
Bharath Ravelli <bharathravelli419@gmail.com>
2021-12-09 00:08:58 +0530

    push ,splice,unshift,filter

--- Desktop/FULL-STACK/assets/app.js
+++ Desktop/FULL-STACK/assets/app.js
@@ -1,29 +1,10 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const add = document.querySelector('#add')
-const multiply= document.querySelector('#multiply')
-const resultbox = document.querySelector('.result')
-console.log(a,b)
-
-/*
-const sum=()=>{
-    const result= parseInt(a.value)+parseInt(b.value)
-    resultbox.innerHTML= result
-}
-add.addEventListener('click',sum)
-const product=()=>{
-    const result= parseInt(a.value)*parseInt(b.value)
-    resultbox.innerHTML= result
-}
-multiply.addEventListener('click',product)
-
- */
-const calculate =(event,operator)=>{
-    console.log(event.target + operator)
-}
-add.addEventListener('click',(event)=>{
-    calculate(event,'add')
+var arr =[1,2,3,4,5]
+arr.splice(1,1)
+arr.unshift(6)
+console.log(arr)
+var new_arr =[]
+new_arr = arr.filter((element,index)=>{
+    if(element%2==0) return true
 })
-multiply.addEventListener('click',(event)=>{
-    calculate(event,'multiply')
-})
+
+console.log(new_arr)
