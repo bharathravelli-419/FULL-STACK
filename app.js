7e5bb62b03e5b63d35cba3aa8a7dc4ea6f1d8031
bharathravelli-419 <bharathravelli419@gmail.com>
2021-12-21 00:53:05 +0530

    paths

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
-div.innerHTML= localStorage.getItem('inputValue')
-const arr = [1,2,3,4,5,"bharath ravelli 419"]
-JSON.stringify(arr)
-console.log(JSON.stringify(arr))
-localStorage.setItem('KEY',JSON.stringify(arr))
- console.log(JSON.parse(localStorage.getItem('KEY')))
-
-
-
-
-
-
-
-
-/*
-document.addEventListener('keyup',(req,res)=>{
-    if(event.keyCode ==13)
-    {
-        localStorage.setItem('inputValue',[1,2,3,4,5])
-           div.innerHTML=input.value
-    }
-})
-
-*/
+const path = require('path')
+console.log(path)
+//console.log(path.resolve('./utils.js') )
+console.log(path.relative('../','./temp/utils.js'))
