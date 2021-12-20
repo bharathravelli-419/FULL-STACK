6b31d798921846cfbaa7bce9b5305b10a3a4d3a0
Bharath Ravelli <bharathravelli419@gmail.com>
2021-12-20 22:14:09 +0530

    LOcal Storage and Related Stuff

--- Desktop/FULL-STACK/app.js
+++ Desktop/FULL-STACK/app.js
@@ -1,5 +1,27 @@
-const {add,subtract,multiply,divide} = require('./utils')
-add()
-subtract()
-multiply()
-divide()
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+div.innerHTML= localStorage.getItem('inputValue')
+const arr = [1,2,3,4,5,"bharath ravelli 419"]
+JSON.stringify(arr)
+console.log(JSON.stringify(arr))
+localStorage.setItem('KEY',JSON.stringify(arr))
+ console.log(JSON.parse(localStorage.getItem('KEY')))
+
+
+
+
+
+
+
+
+/*
+document.addEventListener('keyup',(req,res)=>{
+    if(event.keyCode ==13)
+    {
+        localStorage.setItem('inputValue',[1,2,3,4,5])
+           div.innerHTML=input.value
+    }
+})
+
+*/
